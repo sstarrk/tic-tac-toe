42a02dbe708b9c0da4306d24429422cabef45bf5
sstarrk <170336793+sstarrk@users.noreply.github.com>
2024-10-23 20:32:31 +0500

    Made displayBoard function for displaying board in the console

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const Gameboard = (() => {
     let board = ["", "", "", "", "", "", "", "", ""];
-    
     const getBoard = () => board;
 
-    return { getBoard };
+    const displayBoard = () => {
+        console.log(` ${board[0]}  |  ${board[1]}  |  ${board[2]}` );
+        console.log("------------");
+        console.log(` ${board[3]}  |  ${board[4]}  | ${board[5]} `);
+        console.log("------------");
+        console.log(` ${board[6]}  |  ${board[7]}  |  ${board[8]} `);
+    }
+    
+    return { getBoard, displayBoard };
 })();
