56b8c9215ab957c43ed6a63833ec17ed8a2bc116
sstarrk <170336793+sstarrk@users.noreply.github.com>
2024-10-24 19:57:38 +0500

    Made factory function to make players

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ const Gameboard = (() => {
     const displayBoard = () => {
         console.log(` ${board[0]}  |  ${board[1]}  |  ${board[2]}` );
         console.log("------------");
-        console.log(` ${board[3]}  |  ${board[4]}  | ${board[5]} `);
+        console.log(` ${board[3]}  |  ${board[4]}  |  ${board[5]} `);
         console.log("------------");
         console.log(` ${board[6]}  |  ${board[7]}  |  ${board[8]} `);
     }
-    
-    return { getBoard, displayBoard };
+
+        return { getBoard, displayBoard };
 })();
+
+function Player(name, sign ) {
+    return { name, sign }
+};
